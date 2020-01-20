a039e990dd33eca1582b473b4fc859dc202c8b77
コ <minatoa284@gmail.com>
2020-01-20 12:26:17 +0900

    ボタン押したらSE鳴るやつ

---
+++ Music/Se.js
@@ -0,0 +1,10 @@
+function Se(i){
+    switch(i){
+        case 1:
+        document.getElementById( 'Se01' ).play() ;
+        break;
+        case 2:
+        document.getElementById('Se02').play();
+        break;
+    }
+}
