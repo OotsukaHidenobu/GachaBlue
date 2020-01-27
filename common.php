c9915f45c4cfbebc288ecd031f003c3b6bb68eba
大塚秀信 <h0i8d2e0@yahoo.co.jp>
2020-01-27 14:56:57 +0900

    common更新

--- common.php
+++ common.php
@@ -1,8 +1,8 @@
 <?php
 function getDB1($sql, $param=[]){
-	$dsn  = 'mysql:dbname=quizdb;host=localhost';
-	$user = 'senpai';
-	$pw   = 'indocurry';
+	$dsn  = 'mysql:dbname=GachaBlueDB;host=localhost';
+	$user = 'my1366';
+	$pw   = 'aalqawwj';
 	$dbh = new PDO($dsn, $user, $pw);
 	$sth = $dbh->prepare($sql);
 	$sth->execute($param);
