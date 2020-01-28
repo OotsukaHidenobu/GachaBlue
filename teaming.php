966f2182cd3636ca457dd7e18b15156d4ee56ce4
大塚秀信 <h0i8d2e0@yahoo.co.jp>
2020-01-28 09:35:26 +0900

    編成更新

--- teaming.php
+++ teaming.php
@@ -292,9 +292,12 @@ $warning = "4キャラ全員編成してください"
 
 
             button.addEventListener("click", (e) => {
+
+            answer.forEach(function (param) {
                 const answer = document.querySelectorAll(".team img.img");
             const button = document.querySelector("#button");
-            answer.forEach(function (param) {
+            a = 0;
+            b = 0;
                 if (param.src == "file:///D:/GitClone/GachaBlue/%E3%82%AC%E3%83%81%E3%83%A3%E3%83%96%E3%83%AB%E7%94%BB%E5%83%8F/Empty/Empty.png") {
 
                     a++;
