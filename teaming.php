f1f290cd7771e4ba31a8260a2beeb0ba62f52335
大塚秀信 <h0i8d2e0@yahoo.co.jp>
2020-02-03 16:04:50 +0900

    更新

--- teaming.php
+++ teaming.php
@@ -233,6 +233,7 @@ $warning = "4キャラ全員編成してください"
     $txt = getDB1('select Skill1Text from Characters where id=1');
     echo "<p> $txt </p>";
     ?>
+    <?= $data1['Image']?>
 	<p>「閉じる」か「背景」をクリックするとモーダルウィンドウを終了します。</p>
 	<p><a id="modal-close" class="button-link">閉じる</a></p>
 	<!-- モーダルウィンドウのコンテンツ終了 -->
