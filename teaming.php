4944c0676427a0af5e716270e9c424a14bc9bb40
大塚秀信 <h0i8d2e0@yahoo.co.jp>
2020-02-05 14:54:58 +0900

    更新

--- teaming.php
+++ teaming.php
@@ -610,8 +610,9 @@ $warning = "4キャラ全員編成してください"
             if (b == 4) {
                 location.href = `Buttle.php?id1=` + encodeURIComponent(id1) +"&id2="+ encodeURIComponent(id2)+"&id3="+ encodeURIComponent(id3)+"&id4="+ encodeURIComponent(id4);
             }
-            else {
-                alert("<?php echo $warning; ?>");
+            else {<?php
+$alert = "<script type='text/javascript'>alert('4キャラ全員編成してください。');</script>";
+echo $alert;
             }
         });
 
