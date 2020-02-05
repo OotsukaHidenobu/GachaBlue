b4e583b137562df26c26d0635ce7fc36a41e9960
大塚秀信 <h0i8d2e0@yahoo.co.jp>
2020-02-05 15:03:41 +0900

    更新

--- teaming.php
+++ teaming.php
@@ -26,6 +26,7 @@ $data9 = getDB1('select * from Characters where id=?',[$id9]);
 $data10 = getDB1('select * from Characters where id=?',[$id10]);
 
 $warning = "4キャラ全員編成してください";
+$alert = "<script type='text/javascript'>alert('こちらは侍エンジニア塾です。');</script>";
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -611,7 +612,7 @@ $warning = "4キャラ全員編成してください";
                 location.href = `Buttle.php?id1=` + encodeURIComponent(id1) +"&id2="+ encodeURIComponent(id2)+"&id3="+ encodeURIComponent(id3)+"&id4="+ encodeURIComponent(id4);
             }
             else {
-                alert("<?php echo $warning; ?>");
+                <?=$alert?>
             }
         });
 
