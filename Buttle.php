20101432f882b6f26726c4cffc24eb43077b4ca1
takahashi masashi <ignis0316@outlook.jp>
2020-02-07 15:01:40 +0900

    ｋ

--- Buttle.php
+++ Buttle.php
@@ -250,7 +250,7 @@ $Skill3Image4 = getDB1('select Skill3Image from Characters where id=?',[$id4]);
             enemyHP.innerHTML -= allPower * (100 / DlimitDebuff);
 			var HPBar = document.getElementById('HPBar');
 			console.log(HPBar);
-			HPBar.width = (parseInt(HPBar.width) * (enemyHP / enemyMaxHP)) +"px";
+			HPBar.width = (parseFloat(HPBar.width) * (enemyHP / enemyMaxHP)) +"px";
             console.log(allPower * (100 / DlimitDebuff));
             if (enemyHP.innerHTML <= 0) {
                 location.href = "./Result_Win.html";
