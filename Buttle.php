bc41290f76ce827ef00de6ecbb1d95250ec66635
takahashi masashi <ignis0316@outlook.jp>
2020-02-07 13:40:19 +0900

    ｋ

--- Buttle.php
+++ Buttle.php
@@ -180,16 +180,16 @@ $Skill3Image4 = getDB1('select Skill3Image from Characters where id=?',[$id4]);
             else {
                 var rand = Math.random() * m_charHPArray.length;
                 if (rand < 1) {
-                    m_charHPArray[0].innerHTML -= (enemyPower * (100 / DlimitBuff[0]));
+                    m_charHPArray[0].innerHTML -= Math.round(enemyPower * (100 / DlimitBuff[0]));
                 }
                 else if (rand < 2) {
-                    m_charHPArray[1].innerHTML -= (enemyPower * (100 / DlimitBuff[1]));
+                    m_charHPArray[1].innerHTML -= Math.round(enemyPower * (100 / DlimitBuff[1]));
                 }
                 else if (rand < 3) {
-                    m_charHPArray[2].innerHTML -= (enemyPower * (100 / DlimitBuff[2]));
+                    m_charHPArray[2].innerHTML -= Math.round(enemyPower * (100 / DlimitBuff[2]));
                 }
                 else if (rand < 4) {
-                    m_charHPArray[3].innerHTML -= (enemyPower * (100 / DlimitBuff[3]));
+                    m_charHPArray[3].innerHTML -= Math.round(enemyPower * (100 / DlimitBuff[3]));
                 }
             }
 
