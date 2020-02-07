bd8f2e6a0ee08c9c4b801cd323e17c7be78fab0c
takahashi masashi <ignis0316@outlook.jp>
2020-02-07 16:34:23 +0900

    ｋ

--- Buttle.php
+++ Buttle.php
@@ -433,8 +433,8 @@ $Skill3Image4 = getDB1('select Skill3Image from Characters where id=?',[$id4]);
 
 		#buffID{
 			position:absolute;
-			top:50px;
-			left: 400px;
+			top:70px;
+			left: 350px;
 		}
 
 
